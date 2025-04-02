5d5ea6d3755733662ab9f6d6a2211249a6df166b
TendriZ <rakarazzani24@gmail.com>
2025-04-02 22:42:32 +0700

    add java

---
+++ login.js
@@ -0,0 +1,17 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var username = document.getElementById("Username").value;
+    var password = document.getElementById("Password").value;   
+    
+    if (username === "viesa24" && password === "paketoganteng1234") {
+        document.getElementById("pesanTambahan").innerText = "Login Berhasil, mengalihkan...";
+        setTimeout(() => {
+            window.location.href = "dashboard.html"; // Redirect ke halaman berikutnya
+        }, 1000);
+    } else {
+        document.getElementById("pesanTambahan").innerText = "Username atau Password salah";
+        setTimeout(() => {
+            window.location.href = "index.html"; // Reinput username and password
+        }, 1000);
+    }
+});
